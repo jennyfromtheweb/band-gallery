0faa8ea8b75301ccc0e1007d83ae93e4da2c6fec
jennyfromtheweb <jennyzhcl@yahoo.com>
2015-08-09 14:11:22 -0700

    changed link in footer to open in new tab

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer>
   <div class="footer">
-  	<span>&#169; <a href="http://jennyfromtheweb.github.io">Jenny Li</a> 2015 </span>
+  	<span>&#169; <a href="http://jennyfromtheweb.github.io" target="_blank">Jenny Li</a> 2015 </span>
   	<br class="clear">
   	<div class="follow">
   		<span class="spotify-logo"></span>
-  		<a href="https://open.spotify.com/user/121937045"> Follow me on Spotify!</a>
+  		<a href="https://open.spotify.com/user/121937045" target="_blank"> Follow me on Spotify!</a>
   	</div>
   <div>
 </footer>
